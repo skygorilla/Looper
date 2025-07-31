2f4289a32abca1df7f7113f87cd40d54989ea980
Dimitri Medvedev <medvedev.croatia@gmail.com>
2025-07-31 11:07:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-
 'use client';
 
 import { UIAutomationPanel } from '@/components/UIAutomationPanel';
+import { useState } from 'react';
 
 export default function Home() {
+  const [starterPrompt, setStarterPrompt] = useState('');
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <UIAutomationPanel />
+      <UIAutomationPanel
+        starterPrompt={starterPrompt}
+        setStarterPrompt={setStarterPrompt}
+      />
     </main>
   );
 }
-
-    
