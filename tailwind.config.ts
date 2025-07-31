e5513ca2cbcbe1308a5e02ca90e4773ab3397f20
Dimitri Medvedev <medvedev.croatia@gmail.com>
2025-07-31 05:18:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
-        code: ['Orbitron', 'monospace'],
+        body: ['"Space Grotesk"', 'sans-serif'],
+        headline: ['"Space Grotesk"', 'sans-serif'],
+        code: ['"Roboto Mono"', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
