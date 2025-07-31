70cb2bb03d5cf045caa1976af27df3c5d6231461
Dimitri Medvedev <medvedev.croatia@gmail.com>
2025-07-31 06:29:51 +0000

    make it 200px x 400px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div style={{ width: '60px', height: '90px', backgroundColor: 'hsl(var(--card))', border: '1px solid hsl(var(--border))', borderRadius: 'var(--radius)' }}></div>
+      <div style={{ width: '200px', height: '400px', backgroundColor: 'hsl(var(--card))', border: '1px solid hsl(var(--border))', borderRadius: 'var(--radius)' }}></div>
     </main>
   );
 }
