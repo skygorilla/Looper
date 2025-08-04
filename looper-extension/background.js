4d21a63573d5a4c6886029ab423541ec70043240
Dimitri Medvedev <medvedev.croatia@gmail.com>
2025-08-04 18:39:43 +0000

    is it autonos as extension?

--- looper-extension/background.js
+++ looper-extension/background.js
@@ -1,11 +1,9 @@
-// Background script for Looper extension - Direct Component Injection
 chrome.action.onClicked.addListener((tab) => {
-  // Check if it's a chrome:// URL and skip
-  if (tab.url && tab.url.startsWith('chrome://')) {
+  if (tab.url && (tab.url.startsWith('chrome://') || tab.url.startsWith('https://chrome.google.com'))) {
+    console.log("Looper cannot run on this page.");
     return;
   }
   
-  // Inject the actual LooperAutopilotAdvanced component directly
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['content-script.js']
